00823e2f788b9bbe3029d676050fc40b757f82e9
Cássio Ávila <cassioavila@protonmail.com>
2025-09-01 12:21:57 -0300

    feat (theme): Use the system theme preference by default

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -1,20 +1,32 @@
 import { writable } from 'svelte/store';
+import { getCurrentWindow } from '@tauri-apps/api/window';
 
-const defaultTheme = 'light';
+type Theme = 'light' | 'dark' | 'auto';
+
+async function getSystemTheme() {
+	try {
+		const theme = await getCurrentWindow().theme();
+		return theme;
+	} catch (error) {
+		console.error('Error getting system theme:', error);
+	}
+}
+
+const defaultTheme = 'auto';
 const initialTheme =
 	typeof localStorage !== 'undefined'
 		? (localStorage.getItem('theme') ?? defaultTheme)
 		: defaultTheme;
 
-export const theme = writable<'light' | 'dark'>(initialTheme as 'light' | 'dark');
+export const theme = writable<Theme>(initialTheme as Theme);
 
-const editorThemeCallbacks = new Set<(theme: 'light' | 'dark') => void>();
+const editorThemeCallbacks = new Set<(theme: Theme) => void>();
 
-theme.subscribe((value) => {
+theme.subscribe(async (value) => {
 	if (typeof localStorage !== 'undefined') {
 		localStorage.setItem('theme', value);
 	}
-
+	if (value == 'auto') value = await getSystemTheme();
 	if (typeof document !== 'undefined') {
 		if (value === 'dark') {
 			document.documentElement.classList.add('dark');
@@ -34,7 +46,11 @@ export function toggleTheme() {
 	theme.update((current) => (current === 'light' ? 'dark' : 'light'));
 }
 
-export function registerEditorThemeCallback(callback: (theme: 'light' | 'dark') => void) {
+await getCurrentWindow().onThemeChanged(({ payload: currentTheme }) => {
+	theme.set(currentTheme);
+});
+
+export function registerEditorThemeCallback(callback: (theme: Theme) => void) {
 	editorThemeCallbacks.add(callback);
 
 	return () => {
