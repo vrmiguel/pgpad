6976832ce7faf0b0b95f087e1798cd22bea18c25
Abdugani Toshmukhamedov <79999469+toshmukhamedov@users.noreply.github.com>
2025-09-16 10:27:16 +0500

    Tab shortcuts (#26)
    * fix: prevent text selection
    
    * feat(menu): add native app menu with tab shortcuts
    
    * fix(menu): use CmdOrControl for tab shortcuts
    
    * fix: clean up menu imports and restrict tab close to middle-click
    
    * refactor(menu): restrict about_metadata to macOS
    
    * refactor(init): conditionally import AboutMetadata for macOS

--- src-tauri/src/init.rs
+++ src-tauri/src/init.rs
@@ -1,4 +1,10 @@
-use tauri::WebviewWindowBuilder;
+#[cfg(target_os = "macos")]
+use tauri::menu::AboutMetadata;
+use tauri::menu::MenuItem;
+use tauri::{
+    menu::{Menu, PredefinedMenuItem, Submenu, WINDOW_SUBMENU_ID},
+    AppHandle, Emitter, WebviewWindowBuilder,
+};
 
 fn init_script() -> String {
     format!(
@@ -50,5 +56,129 @@ pub fn build_window(app: &tauri::App) -> tauri::Result<()> {
 
     window_builder.build()?;
 
+    Ok(())
+}
+
+pub fn build_menu(app: &tauri::App) -> anyhow::Result<()> {
+    let app_handle = app.handle();
+    #[cfg(target_os = "macos")]
+    let pkg_info = app_handle.package_info();
+    #[cfg(target_os = "macos")]
+    let about_metadata = {
+        let config = app_handle.config();
+        AboutMetadata {
+            name: Some(pkg_info.name.clone()),
+            version: Some(pkg_info.version.to_string()),
+            copyright: config.bundle.copyright.clone(),
+            authors: config.bundle.publisher.clone().map(|p| vec![p]),
+            ..Default::default()
+        }
+    };
+
+    let window_menu = Submenu::with_id_and_items(
+        app_handle,
+        WINDOW_SUBMENU_ID,
+        "Window",
+        true,
+        &[
+            &PredefinedMenuItem::minimize(app_handle, None)?,
+            &PredefinedMenuItem::maximize(app_handle, None)?,
+            #[cfg(target_os = "macos")]
+            &PredefinedMenuItem::separator(app_handle)?,
+            &PredefinedMenuItem::close_window(app_handle, None)?,
+        ],
+    )?;
+
+    let menu = Menu::with_items(
+        app_handle,
+        &[
+            #[cfg(target_os = "macos")]
+            &Submenu::with_items(
+                app_handle,
+                pkg_info.name.clone(),
+                true,
+                &[
+                    &PredefinedMenuItem::about(app_handle, None, Some(about_metadata))?,
+                    &PredefinedMenuItem::separator(app_handle)?,
+                    &PredefinedMenuItem::services(app_handle, None)?,
+                    &PredefinedMenuItem::separator(app_handle)?,
+                    &PredefinedMenuItem::hide(app_handle, None)?,
+                    &PredefinedMenuItem::hide_others(app_handle, None)?,
+                    &PredefinedMenuItem::separator(app_handle)?,
+                    &PredefinedMenuItem::quit(app_handle, None)?,
+                ],
+            )?,
+            &Submenu::with_items(
+                app_handle,
+                "File",
+                true,
+                &[
+                    &MenuItem::with_id(
+                        app_handle,
+                        "new_tab",
+                        "New Tab",
+                        true,
+                        Some("CmdOrControl+N"),
+                    )?,
+                    &MenuItem::with_id(
+                        app_handle,
+                        "close_tab",
+                        "Close Tab",
+                        true,
+                        Some("CmdOrControl+W"),
+                    )?,
+                ],
+            )?,
+            &Submenu::with_items(
+                app_handle,
+                "Edit",
+                true,
+                &[
+                    &PredefinedMenuItem::undo(app_handle, None)?,
+                    &PredefinedMenuItem::redo(app_handle, None)?,
+                    &PredefinedMenuItem::separator(app_handle)?,
+                    &PredefinedMenuItem::cut(app_handle, None)?,
+                    &PredefinedMenuItem::copy(app_handle, None)?,
+                    &PredefinedMenuItem::paste(app_handle, None)?,
+                    &PredefinedMenuItem::select_all(app_handle, None)?,
+                ],
+            )?,
+            #[cfg(target_os = "macos")]
+            &Submenu::with_items(
+                app_handle,
+                "View",
+                true,
+                &[&PredefinedMenuItem::fullscreen(app_handle, None)?],
+            )?,
+            &window_menu,
+        ],
+    )?;
+
+    app.set_menu(menu)?;
+    app.on_menu_event(move |handle: &tauri::AppHandle, event| {
+        let event = event.id().0.as_str();
+
+        log::debug!("[on_menu_event][{event}] Event triggered");
+
+        if let Err(err) = menu_event_handler(event, handle) {
+            log::error!("[on_menu_event] [{event}] {:?}", err);
+        }
+    });
+
+    Ok(())
+}
+
+fn menu_event_handler(event: &str, handle: &AppHandle) -> anyhow::Result<()> {
+    match event {
+        "new_tab" => {
+            handle.emit("new_tab", ())?;
+        }
+        "close_tab" => {
+            handle.emit("close_tab", ())?;
+        }
+        _ => {
+            log::info!("Unexpected menu event: {}", event);
+        }
+    }
     Ok(())
 }
